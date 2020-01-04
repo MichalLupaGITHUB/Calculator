454e409c086101bbc5a3ab1c4efd3438b9d8dc1c
MichalLupa <michal.rafal.lupa@gmail.com>
2020-01-04 23:50:50 +0100

    Dodanie projektu Calculator

---
+++ Kalkulator/Plansza.cpp
@@ -0,0 +1,22 @@
+﻿#include "Plansza.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+
+int main(array<System::String ^> ^args)
+{
+	// Uaktywnienie wizualnego trybu aplikacji
+	Application::EnableVisualStyles();
+
+	// Ustawienie że aplikacja nie będzie korzystała z www
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	// Uruchomienie aplikacji
+	Kalkulator::Plansza formularz;
+	Application::Run(%formularz);
+
+	return 0;
+}
+
---
+++ Kalkulator/Pomoc.h
@@ -0,0 +1,132 @@
+﻿#pragma once
+
+namespace Kalkulator {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Pomoc
+	/// </summary>
+	public ref class Pomoc : public System::Windows::Forms::Form
+	{
+	public:
+		Pomoc(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Pomoc()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Label^  lblInformacja;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::WebBrowser^  webBrowser1;
+
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->lblInformacja = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->webBrowser1 = (gcnew System::Windows::Forms::WebBrowser());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(102, 220);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Zamknij";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Pomoc::button1_Click);
+			// 
+			// lblInformacja
+			// 
+			this->lblInformacja->AutoSize = true;
+			this->lblInformacja->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lblInformacja->Location = System::Drawing::Point(65, 47);
+			this->lblInformacja->Name = L"lblInformacja";
+			this->lblInformacja->Size = System::Drawing::Size(53, 20);
+			this->lblInformacja->TabIndex = 1;
+			this->lblInformacja->Text = L"label1";
+			this->lblInformacja->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Enabled = false;
+			this->textBox1->Location = System::Drawing::Point(36, 104);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(191, 81);
+			this->textBox1->TabIndex = 2;
+			this->textBox1->Text = L"Autor\r\nAutor: Michal\r\nRok prod: 2019";
+			this->textBox1->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			// 
+			// webBrowser1
+			// 
+			this->webBrowser1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->webBrowser1->Location = System::Drawing::Point(0, 0);
+			this->webBrowser1->MinimumSize = System::Drawing::Size(20, 20);
+			this->webBrowser1->Name = L"webBrowser1";
+			this->webBrowser1->Size = System::Drawing::Size(477, 384);
+			this->webBrowser1->TabIndex = 3;
+			this->webBrowser1->Url = (gcnew System::Uri(L"http://www.wp.pl", System::UriKind::Absolute));
+			// 
+			// Pomoc
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(477, 384);
+			this->Controls->Add(this->webBrowser1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->lblInformacja);
+			this->Controls->Add(this->button1);
+			this->Name = L"Pomoc";
+			this->Text = L"Pomoc";
+			this->Load += gcnew System::EventHandler(this, &Pomoc::Pomoc_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		this->Close();
+	}
+
+	private: System::Void Pomoc_Load(System::Object^  sender, System::EventArgs^  e) {
+		lblInformacja->Text = "Program kalkulator \n Autor: Michal \n Rok: 2019";
+	}
+	};
+}
